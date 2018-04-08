1e83dc0b6d2369f00e9326d57fcf3a678b8e59a1
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-04-08 13:52:11 -0700

    fix: debugging

--- scripts/lifecycle.js
+++ scripts/lifecycle.js
@@ -1,6 +1,7 @@
 function run(pluginConfig, options) {
   const path = require('path')
   const pjson = require(path.join(process.cwd(), 'package.json'))
+  console.dir(pjson)
   const exec = require('./exec')
   process.env.OCLIF_NEXT_VERSION = options.nextRelease.version
   const cmd = pjson.scripts[pluginConfig.script]
