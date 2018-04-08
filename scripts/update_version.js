0d1777640e0ac145bed56ea01ff9918d0d398404
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-04-08 14:02:52 -0700

    fix: remove console.log

--- scripts/update_version.js
+++ scripts/update_version.js
@@ -2,8 +2,6 @@ exports.prepare = (_, options) => {
   const fs = require('fs-extra')
   const path = require('path')
   const pjson = require(path.join(process.cwd(), 'package.json'))
-  console.dir(pjson)
   pjson.version = options.nextRelease.version
   fs.outputJSONSync('package.json', pjson, {spaces: 2})
-  console.log(fs.readFileSync('package.json', 'utf8'))
 }
